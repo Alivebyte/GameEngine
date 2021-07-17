fcbd12936ae35954332be7fbc45cbbcf1459ddf0
Alivebyte <alivebyte.yt@gmail.com>
2021-07-17 11:55:06 +0300

    added glad

--- GayEngin/main.cpp
+++ GayEngin/main.cpp
@@ -1,6 +1,28 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World!";
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to init GLAD" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "GayEngin", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+	glfwMakeContextCurrent(window);
 }
