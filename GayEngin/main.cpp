abd1ac34be774e70bee6f047329ffe3b0a67a32b
Alivebyte <alivebyte.yt@gmail.com>
2021-07-17 13:55:22 +0300

    made some shit

--- GayEngin/main.cpp
+++ GayEngin/main.cpp
@@ -1,22 +1,19 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "main.h"
 #include <iostream>
+
+
 
 int main()
 {
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to init GLAD" << std::endl;
-		return EXIT_FAILURE;
-	}
-
 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
+	
 	GLFWwindow* window = glfwCreateWindow(800, 600, "GayEngin", NULL, NULL);
 	if (window == NULL)
 	{
@@ -25,4 +22,42 @@ int main()
 		return EXIT_FAILURE;
 	}
 	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to init GLAD" << std::endl;
+		return EXIT_FAILURE;
+	};
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//glViewport(0, 0, 400, 400);
+
+	
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		//render order
+		glClearColor(0.2f, 0.8f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
